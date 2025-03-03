d86c8a7d3316194f76ce311fc9a2ee89df209bb9
Yong <neilli1992@gmail.com>
2025-03-03 23:00:44 +0000

    rename trickle-sync to trickle-pull

--- trickle-pull.sh
+++ trickle-pull.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-# Manual sync override: Forces a git pull immediately.
+# Forces a git pull immediately.
 
-LAST_SYNC_FILE="$HOME/.task/last_pull_time"
-
+LAST_PULL_FILE="$HOME/.task/last_pull_time"
 
 echo "\n"
-echo "----- Manual trickle-sync triggered at $(date) -----"
+echo "----- Trickle-pull triggered at $(date) -----"
 echo "\n"
 
 cd ~/taskwarrior-sync-data || { echo "Failed to cd to repo directory"; exit 1; }
 
 # Force git pull
 if git pull --no-rebase; then
-    echo $(date +%s) > "$LAST_SYNC_FILE"  # Update last pull timestamp
-    echo "Manual sync completed."
+    echo $(date +%s) > "$LAST_PULL_FILE"  # Update last pull timestamp
+    echo "Trickle-pull completed."
 else
     echo "Failed to pull changes. Please check for conflicts."
     exit 1
@@ -22,5 +21,5 @@ fi
 # Import tasks.json into Taskwarrior
 task rc.hooks=off rc.json.array=on import tasks.json
 
-echo "Trickle sync completed successfully."
+echo "Trickle pull completed successfully."
 exit 0
